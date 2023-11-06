a5d6c04accad9011efc3fbff693da42678d85425
Filip Žitný <filipzitny@gmail.com>
2023-11-06 11:30:29 +0100

    unit test

--- Tests/core/test_gcd.py
+++ Tests/core/test_gcd.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """File for unit test for gcd."""
-import pytest
 import dispy.core.gcd
 
 
@@ -8,3 +7,5 @@ def test_gcd():
     """Test for gcd."""
     number = dispy.core.gcd.gcd(8, 4)
     assert number == 4
+
+# TODO: Add test for random gcd.
